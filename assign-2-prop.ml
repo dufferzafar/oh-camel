77b4520b78feaf2679157d2c1602ec7307bd997d
Shadab Zafar <dufferzafar0@gmail.com>
2017-09-09 19:20:17 +0530

    Prop: letters with set union

--- assign-2-prop.ml
+++ assign-2-prop.ml
@@ -31,8 +31,25 @@ let rec print_set_tuple fmt s =
 
 let rec print_set_int = print_set print_int;;
 let rec print_set_char = print_set print_char;;
+let rec print_set_str = print_set print_string;;
 
 let print_int i = print (string_of_int i);;
+
+(*
+    Set related helper functions
+*)
+
+let member x s = List.mem x s;;
+let rec union s1 s2 =
+    match s1 with
+        [] -> s2
+    | h::t ->
+        if member h s2 then
+            union t s2
+        else
+            h :: (union t s2)
+;;
+
 
 (*
     =====================================================
@@ -59,8 +76,7 @@ type prop =
 let rec height p =
     match p with
     | P _              -> 0
-    | T
-    | F                -> 0
+    | T | F            -> 0
     | Not p1           -> 1 + height(p1)
     | And (p1, p2)
     | Or (p1, p2)
@@ -77,8 +93,7 @@ let rec height p =
 let rec size p =
     match p with
     | P _              -> 1
-    | T
-    | F                -> 1
+    | T | F            -> 1
     | Not p1           -> 1 + size(p1)
     | And (p1, p2)
     | Or (p1, p2)
@@ -87,12 +102,34 @@ let rec size p =
 
 
 (*
-    Examples of some propositions.
+    letters: prop -> string set
+
+    returns the set of propositional variables
+    that appear in a proposition
+*)
+
+(* TODO: Use "real" sets instead of lists? *)
+let rec letters p =
+    match p with
+    | P p1             -> [p1]
+    | T | F            -> []
+    | Not p1           -> letters p1
+    | And (p1, p2)
+    | Or (p1, p2)
+    | Implies (p1, p2) -> union (letters p1) (letters p2)
+;;
+
+
+(*
+    Examples of some propositions
+
+    Used for testing throughout the assignment
 *)
 
 (* ((a ^ b) v (c ^ d)) *)
 
-let example_1 = Or( And( P("a"), P("b") ), And( P("c"), P("d") ) );;
+let example_1 = Or( And( P("a"), P("b") ), And( P("c"), P("a") ) );;
 
 print_int (size example_1);;
 print_int (height example_1);;
+print_set_str (letters example_1);;
