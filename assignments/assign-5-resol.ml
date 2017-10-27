e76c296c5163a811a3eb3c24facbd79aff3f14ca
Shadab Zafar <dufferzafar0@gmail.com>
2017-10-27 16:25:25 +0530

    resol: where are we going?

--- assignments/assign-5-resol.ml
+++ assignments/assign-5-resol.ml
@@ -17,38 +17,77 @@ type goal = atom list;;
 *)
 
 (* Solve using a stack for goals *)
-let rec solve_dfs program goal_stack  =
+let rec solve_dfs program goals_stack  =
 
     match program with
 
     | [] ->
-        Stack.length goal_stack = 0
+        Stack.length goals_stack = 0
 
-    | hd::tl ->
-        solve_dfs tl goal_stack;;
+    | cls::rest ->
+
+        match cls with
+
+        | Fact h -> true
+        | Rule (h, b) -> false;;
+
+        (* solve_dfs rest goals_stack;; *)
 
 (*
-    solve : program * goal -> bool
+    solve : program -> goal -> bool
 *)
-let solve program goal =
+let solve program goals =
 
     (* Use a standard stack and add all starting goals to subgoals *)
-    let subgoals = Stack.create () in
-        List.iter (fun p -> Stack.push p subgoals) goal;
+    let goals_stack = Stack.create () in
+        List.iter (fun p -> Stack.push p goals_stack) goals;
 
-    solve_dfs program subgoals;;
+    solve_dfs program goals_stack;;
 
+(*
+    Get head of a clause
+
+    get_head : clause -> head
+*)
+let get_head cls = match cls with Fact h | Rule (h, _) -> h
+
+(*
+    Find all clauses whose head matches the goal
+
+    find_matches : program -> goal -> bool
+*)
+let find_matching program goal =
+    List.filter (fun cls -> get_head cls = goal) program
+;;
 
 (*
     Test Cases
 *)
 
-let c1 = Fact (P "S");;
-let c2 = Fact (P "T");;
-let c3 = Rule (P "Q", [P "S"; P "T"]);;
+let f1 = Fact (P "s");;
+let f2 = Fact (P "t");;
+let f3 = Fact (P "u");;
 
-let p1 = [c1; c2; c3];;
+let r1 = Rule (P "q", [P "s"; P "t"]);;
+let r2 = Rule (P "q", [P "t";]);;
 
-let g1 = [P "S"; P "Q"];;
+let r3 = Rule (P "r", [P "z"]);;
 
+let p1 = [f1; f2; f3; r1; r2];;
+
+let g1 = [P "s"; P "q"];;
+
+let g2 = [P "z"];;
+
+(* Should return true *)
 solve p1 g1;;
+
+(* Should return false *)
+solve p1 g2;;
+
+(* Should return true *)
+solve [] [];;
+solve p1 [];;
+
+(* Should return false *)
+solve [] g1;;
