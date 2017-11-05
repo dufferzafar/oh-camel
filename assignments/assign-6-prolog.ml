30029ee71ed776fb6bae5b8479ce145925b97196
Shadab Zafar <dufferzafar0@gmail.com>
2017-11-05 20:30:02 +0530

    Convert atomic_formula to Node of ...

--- assignments/assign-6-prolog.ml
+++ assignments/assign-6-prolog.ml
@@ -22,7 +22,7 @@ type term =
     | Node of predicate * (term list)
 ;;
 
-type atomic_formula = predicate * (term list);;
+type atomic_formula = Node of predicate * (term list);;
 
 (* H of ? *)
 type head = atomic_formula;;
@@ -209,77 +209,77 @@ and solve_one program goal =
 (* Facts *)
 
 (* let t1 = ("wizard", [Node("harry", [])]) *)
-let wz1 = Fact("wizard", [C "harry"]);;
-let wz2 = Fact("wizard", [C "ron"]);;
-let wz3 = Fact("wizard", [C "arthur"]);;
-let wz4 = Fact("wizard", [C "bill"]);;
-let wz5 = Fact("wizard", [C "james"]);;
-let wz6 = Fact("wizard", [C "snape"]);;
+let wz1 = Fact( Node( "wizard", [C "harry"]) );;
+let wz2 = Fact( Node( "wizard", [C "ron"]) );;
+let wz3 = Fact( Node( "wizard", [C "arthur"]) );;
+let wz4 = Fact( Node( "wizard", [C "bill"]) );;
+let wz5 = Fact( Node( "wizard", [C "james"]) );;
+let wz6 = Fact( Node( "wizard", [C "snape"]) );;
 
-let wt1 = Fact("witch", [C "hermione"]);;
-let wt2 = Fact("witch", [C "ginny"]);;
-let wt3 = Fact("witch", [C "molly"]);;
-let wt4 = Fact("witch", [C "fleur"]);;
-let wt5 = Fact("witch", [C "lilly"]);;
+let wt1 = Fact( Node( "witch", [C "hermione"]) );;
+let wt2 = Fact( Node( "witch", [C "ginny"]) );;
+let wt3 = Fact( Node( "witch", [C "molly"]) );;
+let wt4 = Fact( Node( "witch", [C "fleur"]) );;
+let wt5 = Fact( Node( "witch", [C "lilly"]) );;
 
-let mr1 = Fact("married", [C "harry"; C "ginny"]);;
-let mr2 = Fact("married", [C "ron"; C "hermione"]);;
-let mr3 = Fact("married", [C "arthur"; C "molly"]);;
-let mr3 = Fact("married", [C "james"; C "lilly"]);;
-let mr3 = Fact("married", [C "bill"; C "fleur"]);;
+let mr1 = Fact( Node( "married", [C "harry"; C "ginny"]) );;
+let mr2 = Fact( Node( "married", [C "ron"; C "hermione"]) );;
+let mr3 = Fact( Node( "married", [C "arthur"; C "molly"]) );;
+let mr3 = Fact( Node( "married", [C "james"; C "lilly"]) );;
+let mr3 = Fact( Node( "married", [C "bill"; C "fleur"]) );;
 
-let lv1 = Fact("loves", [C "james"; C "lilly"]);;
-let lv2 = Fact("loves", [C "snape"; C "lilly"]);;
-let lv3 = Fact("loves", [C "harry"; C "ginny"]);;
-let lv4 = Fact("loves", [C "ron"; C "hermione"]);;
+let lv1 = Fact( Node( "loves", [C "james"; C "lilly"]) );;
+let lv2 = Fact( Node( "loves", [C "snape"; C "lilly"]) );;
+let lv3 = Fact( Node( "loves", [C "harry"; C "ginny"]) );;
+let lv4 = Fact( Node( "loves", [C "ron"; C "hermione"]) );;
 
-let pt1 = Fact("parent", [C "arthur"; C "ron"]);;
-let pt2 = Fact("parent", [C "arthur"; C "ginny"]);;
-let pt3 = Fact("parent", [C "arthur"; C "bill"]);;
-let pt4 = Fact("parent", [C "molly"; C "ron"]);;
-let pt5 = Fact("parent", [C "molly"; C "ginny"]);;
-let pt6 = Fact("parent", [C "molly"; C "bill"]);;
-let pt7 = Fact("parent", [C "james"; C "harry"]);;
-let pt8 = Fact("parent", [C "lilly"; C "harry"]);;
+let pt1 = Fact( Node( "parent", [C "arthur"; C "ron"]) );;
+let pt2 = Fact( Node( "parent", [C "arthur"; C "ginny"]) );;
+let pt3 = Fact( Node( "parent", [C "arthur"; C "bill"]) );;
+let pt4 = Fact( Node( "parent", [C "molly"; C "ron"]) );;
+let pt5 = Fact( Node( "parent", [C "molly"; C "ginny"]) );;
+let pt6 = Fact( Node( "parent", [C "molly"; C "bill"]) );;
+let pt7 = Fact( Node( "parent", [C "james"; C "harry"]) );;
+let pt8 = Fact( Node( "parent", [C "lilly"; C "harry"]) );;
 
 (* Rules: head :- body *)
 let r1 = Rule(
-    ("father", [V "F"; V "C"]),
+    Node("father", [V "F"; V "C"]),
     [
-        ("wizard", [V "F"]);
-        ("parent", [V "F"; V "C"])
+        Node("wizard", [V "F"]);
+        Node("parent", [V "F"; V "C"])
     ]
 );;
 
 let r2 = Rule(
-    ("mother", [V "M"; V "C"]),
+    Node("mother", [V "M"; V "C"]),
     [
-        ("witch", [V "M"]);
-        ("parent", [V "M"; V "C"])
+        Node("witch", [V "M"]);
+        Node("parent", [V "M"; V "C"])
     ]
 );;
 
 let r3 = Rule(
-    ("son", [V "S"; V "P"]),
+    Node("son", [V "S"; V "P"]),
     [
-        ("wizard", [V "S"]);
-        ("parent", [V "P"; V "S"])
+        Node("wizard", [V "S"]);
+        Node("parent", [V "P"; V "S"])
     ]
 );;
 
 let r4 = Rule(
-    ("daughter", [V "D"; V "P"]),
+    Node("daughter", [V "D"; V "P"]),
     [
-        ("witch", [V "D"]);
-        ("parent", [V "P"; V "D"])
+        Node("witch", [V "D"]);
+        Node("parent", [V "P"; V "D"])
     ]
 );;
 
 (* Another definition of loves *)
 let r5 = Rule(
-    ("loves", [V "X"; V "Y"]),
+    Node("loves", [V "X"; V "Y"]),
     [
-        ("married", [V "X"; V "Y"])
+        Node("married", [V "X"; V "Y"])
     ]
 )
 
@@ -300,17 +300,21 @@ let p1 = [
 (* Goals *)
 
 (* True / False ? *)
-let g0 = ("loves", [C "snape"; C "lilly"])
-let g1 = ("loves", [C "bill"; C "fleur"])
+
+(* Comes directly from the facts *)
+let g0 = Node("loves", [C "snape"; C "lilly"])
+
+(* Comes from the rule involving marriage  *)
+let g1 = Node("loves", [C "bill"; C "fleur"])
 
 (* Who is father of harry? *)
-let g1 = ("father", [V "X", C "harry"]);;
+let g1 = Node("father", [V "X"; C "harry"]);;
 
 (* Who loved lilly? *)
-let g2 = ("loves", [V "Z", C "lilly"]);;
+let g2 = Node("loves", [V "Z"; C "lilly"]);;
 
 (* Who loves each other? *)
-let g3 = ("loves", [V "Z", V "T"]);;
+let g3 = Node("loves", [V "Z"; V "T"]);;
 
 (*
     The answer can be true/false OR a substitution list.
